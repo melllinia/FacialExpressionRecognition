7cdc4057e6232df65e2965b71dead56ff71995bd
Melina <mm224xr@student.lnu.se>
2024-05-21 01:07:51 +0400

    Minor changes to ImageProcessor

--- src/image_processing.py
+++ src/image_processing.py
@@ -8,7 +8,8 @@ class ImageProcessor:
     def find_faces(self):
         gray_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-        self.faces = face_classifier.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))
+        faces = face_classifier.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))
+        self.faces = {face_id: [x, y, w, h] for face_id, (x, y, w, h) in enumerate(faces)}
 
     def show_faces(self):
         cv2.imshow('Faces detected', self.img)
@@ -17,16 +18,23 @@ class ImageProcessor:
 
     def get_cropped_faces(self):
         if self.faces is None:
-            self.find_faces()
-        return {i: self.img[y:y + h, x:x + w] for i, (x, y, w, h) in enumerate(self.faces)}
+            return None
+
+        return {face_id: self.img[y:y + h, x:x + w] for face_id, (x, y, w, h) in self.faces.items()}
 
     def add_labels(self, labels):
-        for i, (x, y, w, h) in enumerate(self.faces):
+        if self.faces is None:
+            return None
+
+        for face_id in self.faces:
+            (x, y, w, h) = self.faces[face_id]
             cv2.rectangle(self.img, (x, y), (x + w, y + h), (0, 255, 0), 1)
-            cv2.putText(self.img, labels[i], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            cv2.putText(self.img, labels[face_id], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        return self.img
 
 
 # processor = ImageProcessor('golden-trio.jpg')
+# processor.find_faces()
 # print(processor.get_cropped_faces())
 # processor.add_labels(['a', 'b', 'c'])
 # processor.show_faces()
