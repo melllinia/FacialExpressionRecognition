9802739ff24d43870bb2a813979ddab8fd55438b
hovogrig <hovo.grigoryan.01@list.ru>
2024-05-21 22:12:21 +0400

    Implemented model and some rest apis

---
+++ server/controllers.py
@@ -0,0 +1,63 @@
+import torch
+import uvicorn
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+emotions = {
+    0 : 'Angry',
+    1 : 'Disgust',
+    2 : 'Fear',
+    3 : 'Happy',
+    4 : 'Neutral',
+    5 : 'Sad',
+    6 : 'Surprise'
+}
+
+model = torch.jit.load('./model/fer_model.pt')
+
+
+@app.get("/model/summary", 
+         summary="Get Model Summary",
+         tags = ["Model Information"])
+def get_model():
+    """
+    Retrieve the summary or evaluation details of the model.
+
+    Returns:
+    - **Dict[str, str]**: A dictionary containing the model's summary or evaluation details.
+    """
+    return {"model" : model.eval()}
+
+
+@app.get("/model/emotions", 
+         tags = ["Model Information"])
+def get_emotions():
+    """
+    This endpoint returns a list of supported emotions that the model can recognize.
+    """
+    return {"supported_emotions": emotions}
+
+
+@app.get("/model/emotions/{emotion_id}", 
+         summary="Get Emotion by id",
+         tags = ["Model Information"])
+def get_emotion_by_id(emotion_id : int):
+    """
+    Retrieve an emotion based on the provided emotion ID.
+
+    Args:
+    - **emotion_id** (int): The ID of the emotion to retrieve. Must be between 0 and 6 (inclusive).
+
+    Raises:
+    - **HTTPException**: If the emotion_id is not in the range 0 to 6, a 400 status code is returned with an appropriate error message.
+
+    Returns:
+    - **Dict[str, str]**: A dictionary containing the emotion corresponding to the provided ID.
+    """
+    if emotion_id < 0 or emotion_id > 6:
+        raise HTTPException(status_code=400, detail="Id must be in the range(0, 6)")
+    return {"emotion": emotions[emotion_id]} 
+
+if __name__ == "__main__":
+    uvicorn.run("controllers:app", host="0.0.0.0", port=8000, reload=True)
